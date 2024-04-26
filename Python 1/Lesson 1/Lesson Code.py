c2c69e2379702ba6e9bb146874a4334e950a1d28
orlandoalexander <itsorlando@outlook.com>
2024-04-26 18:52:07 +0100

    Lesson 1

---
+++ Python 1/Lesson 1/Lesson Code.py
@@ -0,0 +1,15 @@
+# Building our very own Alexa
+
+# Task 1 - Boot up our Alexa
+print("Starting operating system...")
+print("Warming up...just a moment")
+print("Updating system")
+
+# Task 2 - Ask user to login
+username = "Jeff Bezos"
+password = input("Enter your password: ")
+email_address = "jeffbezos@outlook.com"
+
+# Create two of your own variables
+    # Don't start your variable name with a number
+    # Make sure there are no spaces in your variable name (use _ instead)
