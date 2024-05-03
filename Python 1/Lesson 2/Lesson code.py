6e2f0fd2d9f045357706ece0dea1b72d895bc685
orlandoalexander <itsorlando@outlook.com>
2024-05-03 18:33:46 +0100

    Lesson 2

---
+++ Python 1/Lesson 2/Lesson code.py
@@ -0,0 +1,41 @@
+# Building our very own Alexa
+
+# Step 1 - Boot up our Alexa
+print("Starting operating system...")
+print("Warming up...just a moment")
+print("Updating system")
+
+# Step 2 - Ask user to login
+username = "Jeff Bezos"
+password = input("Enter your password: ")
+email_address = "jeffbezos@outlook.com"
+
+
+# Step 3 - Store numbers in variables
+num1 = "12"
+num2 = "10"
+
+print(num1+num2)
+
+
+# Step 4 - Store Boolean in variables
+logged_in = True
+super_user = False
+
+
+
+# Step 5 - Ask user what their age is
+age = int(input("Enter your age: "))
+birthday = input(f"Hello {username}, when will you be {age+1}?")
+
+
+# Step 6 - Check if the player is old enough to use Alexa
+if age > 14:
+    print("Welcome, you are old enough to use Alexa!")
+
+
+
+
+
+
+
