d1ca82042df8af6eb4c48c217c571704ff57dc01
orlandoalexander <itsorlando@outlook.com>
2024-06-21 16:57:13 +0100

    Lesson 8

---
+++ Python 3/Lesson 8/Lesson code.py
@@ -0,0 +1,11 @@
+class Wizard:
+    def __init__(self): # method
+        self.name = None # attribute
+        self.birth_year = None
+        self.house = None
+        self.wand = None
+        self.favourite_teacher = None
+        # Create more attributes
+
+harry = Wizard() # initialising an object of the Wizard class
+hermione = Wizard()
